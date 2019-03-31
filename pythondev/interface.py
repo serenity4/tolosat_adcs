89b84ec343ad39a07484286d85b9ed3571154ccc
belmant <belmant@etud.insa-toulouse.fr>
2019-03-31 22:48:11 +0200

    Added interfaced python

--- pythondev/interface.py
+++ pythondev/interface.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matlab.engine
+import os
 
 # ref = matlab.engine.find_matlab()
 # print("Session ID found: {}".format(ref[0]))
@@ -12,9 +13,10 @@ eng = matlab.engine.start_matlab("-desktop")
 ### starts the engine and opens it
 eng.eval('addpath(genpath("PILIA"))')
 ### add PILIA to path as it tends to slip away from MATLAB's path
+eng.eval('cd PILIA/PROJECTS/tolosat_adcs_kalman/V1/CONF', nargout=0)
 ConfParam = eng.eval('Main_ConfLaunch()')
 eng.workspace['ConfParam'] = ConfParam
-### builds configuration parameters
+## builds configuration parameters
 eng.open_system(model_path, 'window', nargout=0)
 ### opens the Simulink model
 model_name = eng.eval('gcs')
@@ -26,6 +28,18 @@ eng.set_param(model_name,'FixedStep','ConfParam.confOrbit.dt', nargout=0);
 eng.set_param(model_name,'SaveTime','off', nargout=0);
 eng.set_param(model_name,'LibraryLinkDisplay','all', nargout=0);
 eng.set_param(model_name,'MultiTaskRateTransMsg','warning', nargout=0)
+print("Welcome to the PILIA python interfacer! Available commands are:\nhelp - displays help\nupdate - update workspace with new configuration variables\nexit - exits the prompt")
 
 while 1:
-    pass
+    command = input().split(sep=" ")
+    if command[0] == 'help':
+        print("haha you got no help")
+    elif command[0] == 'update':
+        eng.eval('clear', nargout=0)
+        ConfParam = eng.eval('Main_ConfLaunch()')
+        eng.workspace['ConfParam'] = ConfParam
+    elif command[0] == 'exec':
+        eng.eval(command[1:])
+    elif command[0] == 'exit':
+        os.system(r'del -Force -Recurse .\PILIA\PROJECTS\tolosat_adcs_kalman\V1\CONF\slprj')
+        exit()
